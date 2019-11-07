cd7988d065064565ec95560a431d55825d27a550
lightionight <lightionight@aol.com>
2019-11-07 23:22:57 +0800

    test

--- Lesson02/test_push.cs
+++ Lesson02/test_push.cs
@@ -7,5 +7,6 @@ class TestPush
         Console.WriteLine("i did it,guys");
         Console.WriteLine("still not  success");
         Console.WriteLine("not ease ");
+        Console.WriteLine("oh my god, i did it");
     }
 }
