103f373abe25f20179c9ef231dc888484484a7f2
lightionight <lightionight@aol.com>
2019-11-12 23:27:56 +0800

    still need to fix

--- Test/Solution.cs
+++ Test/Solution.cs
@@ -1,35 +1,39 @@
 using System;
 
-namespace Solutions
-{
-    class Solution 
-    {
-        public void TwoSum(int[] nums, int target)
-        {
-            //寻求数组中所有两个数字的和等于一个特定的数;
-            int[] result = new int[(nums.Length)];
-            int addNumber = 0;
-            int addTwoNumber = addNumber + 1;
-            for(int outer = 0; outer <nums.Length - 1; outer++) {
-                for(int inner = 1; inner < nums.Length - 1; inner++) {
-                    if(target == (nums[outer] + nums[inner])) {
-                        result[addNumber] = nums[outer];
-                        result[addTwoNumber] = nums[inner];
-                        addNumber++;
-                        addTwoNumber++;
+namespace Solutions {
+
+
+    class Solution {
+        public void TwoSum(int[] nums, int target) {
+            int[] result = new int[nums.Length];//新建结果数组
+            for(int index1 = 0; index1 < nums.Length - 1; index1++)
+            {
+                for(int index2 = index1 + 1; index2 < nums.Length; index2++)
+                {
+                    if(nums[index1] == target - (nums[index2]))
+                    {
+                        result[index1] = nums[index1];
+                        result[index2] = nums[index2];
                     }
                 }
             }
-
-            Console.Write("Result is : [");
+            //打印结果
+            Console.Write("Result is: [");
             foreach (var item in result)
             {
-                if(item != 0) {
-                    Console.Write($" {item}, ");
+                if(item != 0)
+                {
+                    if(item != result[result.Length - 1])
+                    {
+                        Console.Write($" {item},");
+                    }else
+                    {
+                        Console.Write($" {item}");
+                    }
                 }
-            
             }
-            Console.Write("].");
+            Console.Write("]");
+
         }
     }
 }
