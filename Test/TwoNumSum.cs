816e9633c4d23d88f41af7a32a0454f666304bba
lightionight <lightionight@aol.com>
2019-11-12 16:36:49 +0800

    fix

--- Test/TwoNumSum.cs
+++ Test/TwoNumSum.cs
@@ -6,9 +6,7 @@ namespace Solutions
     {
         public void TwoSum(int[] nums, int target)
         {
-            //初始化数组和target
-             nums = new [] {2, 7, 11, 15};
-             target = 9;
+            //寻求数组中所有两个数字的和等于一个特定的数;
              foreach (var number in nums)
              {
                  for (int i = 0; i < nums.Length; i++)
@@ -22,22 +20,6 @@ namespace Solutions
                      }
                  }
              }
-
-            
-            //遍历数列中的每一个数
-            // for(int a = 0; a < (nums.Length - 1); a++)
-            // {
-            //     for(int b = 1; b < nums.Length; b++)
-            //     {
-            //         if(target == (nums[a] + nums[b]))
-            //         {
-            //             Console.WriteLine($"The target can combile with {nums[a]} and {nums[b]}");
-                        
-            //         }
-            //     }
-            // }
-
-
         }
     }
 }
