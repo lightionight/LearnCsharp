0461490de9e0df68ab80b4a7be03dfccc4e9a709
lightionight <lightionight@aol.com>
2019-11-14 22:01:43 +0800

    test

--- Test/LongSubstring.cs
+++ Test/LongSubstring.cs
@@ -4,35 +4,7 @@ using System.Collections;
 namespace Test {
     public class LongSubstring{
         public int LengthOfLongestSubstring(string s) {
-            ArrayList result = new ArrayList();
-            int finnal = 0;
-            int start = 0;
-            int i = 0;
-            foreach (var item in s)
-            {
-                if(!result.Contains(item)) {
-                    result.Add(item);
-                    start = result.Count;
-                }
-                else if(result.Count >= finnal) {
-                    finnal = result.Count;
-                    i++;
-                    result.Clear();
-                    result.Add(item);
-                }
-                else {
-                    result.Clear();
-                    result.Add(item);
-                }
-                    
-            }
-            if(start <= finnal)
-                {
-                    return finnal;
-                }
-            else {
-                return start;
-            }
+             
         }
     }
 
