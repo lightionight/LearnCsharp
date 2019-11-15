785d7704dae613fad8f0302251b52159083edebb
lightionight <lightionight@aol.com>
2019-11-15 10:37:22 +0800

    Test 04

---
+++ Test/4_MedianOfTwoArrays.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Test
+{
+    public class MedianOfTwoArrays 
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
+        {
+            
+            //合并两个数组num1和Num2;
+            int[] all = new int[nums1.Length + nums2.Length];
+            nums1.CopyTo(all, 0);
+            nums2.CopyTo(all, nums1.Length);
+            Array.Sort(all);
+            
+            //判断数值奇偶性并求出中值;
+            double result = 0;
+            if (all.Length % 2 == 0)
+            {
+                result = (all[all.Length / 2 - 1] + all[all.Length / 2]) / 2;
+            }
+            else
+            {
+                result = all[all.Length / 2];
+            }
+
+            //返回计算结果
+            return result;
+            
+        }
+    }
+}
