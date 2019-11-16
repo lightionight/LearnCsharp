cf488b97a6df3135b0222c01d35230af3d1d50f2
lightionight <lightionight@aol.com>
2019-11-16 20:29:29 +0800

    test 05 done

--- Test/5_LongestPalindromicSubstring.cs
+++ Test/5_LongestPalindromicSubstring.cs
@@ -4,43 +4,46 @@ using System.Collections.Generic;
 
 namespace Test
 {
-    public class LongestPalindromeSubstring 
+    public class LongestPalindromeSubstring
     {
-        public String LongestPalindrome(string s) 
+        public String LongestPalindrome(string s)
         {
-               
-            int start = 0;
-            int end   = 0;
-            int count = 0;
 
-           for(int numStart = 0; numStart < s.Length; numStart++)
-           {
-               for(int numEnd = numStart + 1; numEnd < s.Length; numEnd++)
-               {   
-                   if(s[numStart] == s[numEnd])
-                   {
-                       start = numStart;
-                       end   = numEnd;
-                       count = numEnd - numStart;
-                       break;
+            int start, count, lastCount;
+                start = count = lastCount = 0;
 
-                   }
-               }
-               if((end - start + 1) <= count)
-               {
-                   start = numStart;
-                   end = numStart + count;
-               }
-               else
-               {
-                   count = 0;
-               }
-           }
+            for (int numStart = 0; numStart < (s.Length - 1); numStart++)
+            {
+                for (int numEnd = numStart + 1; numEnd < s.Length; numEnd++)
+                {
+                    if (s[numStart] == s[numEnd])
+                    {
+                        count = numEnd - numStart + 1; //统计上一次的位数
+                        break;
+                    }
+                }
 
-           //返回结果
-           String finnal = s.Substring(start, (end - start));
-           return finnal;
-           
+
+                if (lastCount < count)
+                {
+                    start = numStart;
+                    lastCount = count;
+
+                }
+                else
+                {
+                    count = 0;
+
+                }
+
+
+            }
+
+            //返回结果
+            String finnal = s.Substring(start, lastCount);
+            return finnal;
+
+
         }
     }
 }
