c051afa4572a26149d39a4e722e4a0c5e9c18ca0
lightionight <lightionight@aol.com>
2019-11-19 00:02:03 +0800

    test 05 是噩梦！

--- LeetCode/5_LongestPalindromicSubstring.cs
+++ LeetCode/5_LongestPalindromicSubstring.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace LeetCode
 {
@@ -8,26 +11,72 @@ namespace LeetCode
         {
 
             //局部变量**************************
-            
+            int index1 = 0;
+            int index2 = 0;
+            int cycle1 = 0;
+            int cycle2 = 0;
+            int countNum1 = 0;
+            int countNum2 = 0;
+            String result = "";
+            String ss = new String(s.ToCharArray().Reverse().ToArray());
             //局部变量**************************
 
             //特殊情况***********************
-            if(s.Length == 0 || s.Length == 1)
+            if(s.Length == 0 || s.Length == 1 || ss == s)
             {
                 result = s;
                 goto jump;
             }
-            //特殊情况***********************
+            //特殊情况**********************
             //*************************正常处理String********************************//
-            
+            for (int i = 0; i < s.Length; i++)
+            {
+                    for (int item = 1; i - item >= 0 && (i + item) < s.Length && s[i - item] == s[i + item]; item++)
+                    {
+                        countNum1++;
+                    }
+                    for (int item = 1; (i + item) < s.Length && s[i] == s[i+item]; item++)
+                    {
+                        countNum2++;
+                    }
+                    if(cycle1 <= countNum1)
+                    {
+                        cycle1 = countNum1;
+                        index1 = i;
+                                               
+                    }
+                    countNum1 = 0;
+                    
+                    if(cycle2 <= countNum2)
+                    {
+                        cycle2 = countNum2;
+                        index2 = i;
+                                               
+                    }
+                    countNum2 = 0;
+                    
+            }
             //第一次下一位与 当前位相等 且与 前一位不相等的情况
-            
+            if(cycle1 >= cycle2)
+            {
+             result = s.Substring((index1 - cycle1), (cycle1 * 2) + 1);
+
+            }
+            else
+            {
+             result = s.Substring((index2), cycle2 + 1);
+
+            }
             //第一次下一位与当前位不相等的情况
-            
+            if(result.Length == 0)
+            {
+                result = s.Substring(0, 1);
+                goto jump;
+            }
             //*************************正常处理String********************************//
+            // result = s.Substring((index - cycle), (cycle * 2) + 1);
             
-            
-            
+            jump:
             // 返回结果
             return result;
             // 返回结果
--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace LearnCsharp
             
             //test——05：最长子回文字符串
             SubstringNew a = new SubstringNew();
-            string[] word = {"abcda"};
+            string[] word = {"tattarrattat"};
             foreach (var item in word)
             {
                 Console.WriteLine(a.LongestSubstring(item));
