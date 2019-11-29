03a857d4b28653f3522e102853e1744fd6688c30
lightionight <lightionight@aol.com>
2019-11-29 16:56:00 +0800

    update

--- LeetCode/8_IntIdentity.cs
+++ LeetCode/8_IntIdentity.cs
@@ -1,22 +1,26 @@
 using System;
-using System.Collections;
 using System.Text.RegularExpressions;
 
 namespace LeetCode
 {
     class IntIdentity
     {
-        public string mySolution(string s)
+        public void mySolution(string s)
         {
+
             private int32 _Min = Int32.MaxValue;
             private int32 _Max = Int32.MinValue;
             private string nullValue = " ";
             private string defultValue = "0";
-            if(s.length == 0)
+            private string word = s;
+            if(string.  == 0)
             {
                 return defultValue;
             }
             else if()
+            {
+                
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## LearnCsharp
+# LearnCsharp
 -----
-### C# language Learn
-Reference Book: Essential C# 7.0</br>
-Test With LeetCode.
+## C# language Learn
+- Reference Book: Essential C# 7.0
+- Test With LeetCode.
