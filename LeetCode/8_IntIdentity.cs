d639a3e22978069c985158aa4911ee79f8605bf8
lightionight <lightionight@aol.com>
2019-11-29 20:46:00 +0800

    本地配置修复

--- LeetCode/8_IntIdentity.cs
+++ LeetCode/8_IntIdentity.cs
@@ -5,43 +5,20 @@ namespace LeetCode
 {
     class IntIdentity
     {
-        public string mySolution(string s)
+        public void mySolution(string str) 
         {
-            Int32 _Max = Int32.MaxValue;
-            Int32 _Min = Int32.MinValue;
+            private static readonly Int32 _Max = Int32.MaxValue;
+            private static readonly Int32 _Min = Int32.MinValue;
             string defultReturn = "0";
             //正向表达式模式;
-            Regex _isAllNull = new Regex("* ");
+            Regex _isAllNull = new Regex("[]");
             Regex _isAllChar = new Regex("* ");
+            Regex _isNum     = new Regex("");
             
 
 
-            if (s.Length == 0 || )
-            {
-                return defultReturn;
-            }
-            else if (true)
-            {
-                
-            }
-
-
-            return s;
+            
         }
-        //正向表达式判断
-        // public bool IsAllNull(string word)
-        // {
-            
-        //     bool result = allNull.IsMatch(word);
-        //     return result;
-        // }
-
-        // public bool IsAllNull(string word)
-        // {
-        //     Regex allNull = new Regex("* ");
-        //     bool result = allNull.IsMatch(word);
-        //     return result;
-        // }
 
     }
 }
