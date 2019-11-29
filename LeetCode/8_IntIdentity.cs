1cbc3695118e61af5c9db85d15609626418f7ab2
lightionight <lightionight@aol.com>
2019-11-29 16:22:13 +0800

    add sln

---
+++ LeetCode/8_IntIdentity.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace LeetCode
+{
+    class IntIdentity
+    {
+        public string mySolution(string s)
+        {
+            private int32 _Min = Int32.MaxValue;
+            private int32 _Max = Int32.MinValue;
+            private string nullValue = " ";
+            private string defultValue = "0";
+            if(s.length == 0)
+            {
+                return defultValue;
+            }
+            else if()
+        }
+    }
+}
