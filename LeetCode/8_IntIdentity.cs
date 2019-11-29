cb54778d25accc4fe99cbd0824fbef526e2bf9c3
lightionight <lightionight@aol.com>
2019-11-29 20:57:36 +0800

    update

--- LeetCode/8_IntIdentity.cs
+++ LeetCode/8_IntIdentity.cs
@@ -3,22 +3,5 @@ using System.Text.RegularExpressions;
 
 namespace LeetCode
 {
-    class IntIdentity
-    {
-        public void mySolution(string str) 
-        {
-            private static readonly Int32 _Max = Int32.MaxValue;
-            private static readonly Int32 _Min = Int32.MinValue;
-            string defultReturn = "0";
-            //正向表达式模式;
-            Regex _isAllNull = new Regex("[]");
-            Regex _isAllChar = new Regex("* ");
-            Regex _isNum     = new Regex("");
-            
-
-
-            
-        }
-
-    }
+    
 }
