0a2b2703556c31e294a45612de7e474bc5cbf104
lightionight <lightionight@aol.com>
2020-01-09 14:16:27 +0800

    LeetCode 08

--- LeetCode/8_IntIdentity.cs
+++ LeetCode/8_IntIdentity.cs
@@ -2,12 +2,13 @@ using System;
 using System.Text.RegularExpressions;
 namespace LeetCode
 {
-    class IntIdentity
+    public class IntIdentity
     {
         private static readonly Int32 _MaxValue = Int32.MaxValue;
         private static readonly Int32 _MinValue = Int32.MinValue;
         Regex _isNull = new Regex(@"[\s]");
-        private string Mysolution(string str)
+        Regex _isWord = new Regex(@"[]");
+        private int Mysolution(string str)
         {
             string defultReturn = "0";
 
@@ -19,7 +20,7 @@ namespace LeetCode
             {
                 
             }
-            return defultReturn;
+            return int.Parse(defultReturn);
         }
     }
 }
